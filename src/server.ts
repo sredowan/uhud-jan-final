aa6963069e2934d446022f4b4d13912a2f83b1aa
Uhud Deploy <deploy@uhudbuilders.com>
2026-01-13 18:09:46 +0600

    Update domain config to mediumvioletred-mandrill-596025.hostingersite.com

--- src/server.ts
+++ src/server.ts
@@ -52,7 +52,7 @@ const PORT = process.env.PORT || 3001;
 
 // Enable CORS
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:3001", "http://localhost:5174", "http://localhost:5175"],
+    origin: ["http://localhost:5173", "http://localhost:3001", "http://localhost:5174", "http://localhost:5175", "https://mediumvioletred-mandrill-596025.hostingersite.com"],
     credentials: true
 }));
 
